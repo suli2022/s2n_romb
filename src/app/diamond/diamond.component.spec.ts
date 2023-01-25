7fbe78bc209b3ee40a230c348ab38dba74f805e4
Sallai András <joska@zold.and>
2023-01-25 10:20:24 +0100

    Számítás működik Enterre

---
+++ src/app/diamond/diamond.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DiamondComponent } from './diamond.component';
+
+describe('DiamondComponent', () => {
+  let component: DiamondComponent;
+  let fixture: ComponentFixture<DiamondComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DiamondComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DiamondComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/diamond/diamond.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-diamond',
+  templateUrl: './diamond.component.html',
+  styleUrls: ['./diamond.component.scss']
+})
+export class DiamondComponent implements OnInit {
+  aside = new FormControl('');
+  alpha = new FormControl('');
+  perimeter = new FormControl('');
+  area = new FormControl('');
+
+  perimeterVisible = false;
+  areaVisible = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onKeyup(event:any) {
+    if (event.key == "Enter") {
+      this.onEnterHandler();
+    }
+  }
+  onEnterHandler() {
+    let aside = Number(this.aside.value);
+    let alpha = Number(this.alpha.value);
+    let perimeter = this.calcPerimeter(aside);
+    let area = this.calcArea(aside, alpha);
+    this.perimeter.setValue(String(perimeter));
+    this.area.setValue(String(area));
+    this.perimeterVisible = true;
+    this.areaVisible = true;    
+  }
+  calcPerimeter(aside : number) {
+    return aside * 4;
+  }
+  calcArea(aside: number, alpha: number) {
+    let rad = alpha * Math.PI / 180;
+    let area = Math.pow(aside, 2) * Math.sin(rad);
+    return area;
+  }
+}
